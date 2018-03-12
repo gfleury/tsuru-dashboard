20c25dc7292083a0457463deac3882e2d825185d
gfleury <gfleury@gmail.com>
2018-03-12 18:00:28 +0100

    Adding missing recursive installed apps

--- abyss/settings.py
+++ abyss/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'tsuru_dashboard',
 ]
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
